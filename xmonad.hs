c73c47c860b86e9532eb2218c9a763b9a68183b7
Toby Dickenson <tbdickenson@googlemail.com>
2024-07-06 01:28:38 +0200

    xmonad updates

--- xmonad.hs
+++ xmonad.hs
@@ -1,18 +1,18 @@
 import XMonad
-import XMonad.Util.EZConfig
-import XMonad.Util.SpawnOnce
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageHelpers
 import XMonad.Actions.SpawnOn
-
--- Layout stuff
+import XMonad.Config.Desktop
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Util.EZConfig
+import XMonad.Util.SpawnOnce
 
 main :: IO ()
 main = xmonad $ ewmhFullscreen . ewmh $ myConfig
 
-myConfig = def 
+myConfig = desktopConfig
     { modMask = mod4Mask
     , terminal = "alacritty"
     , layoutHook = myLayoutHook
@@ -21,44 +21,45 @@ myConfig = def
     , startupHook = myStartupHook
     , focusFollowsMouse = False
     }
-    `additionalKeysP` 
+    `additionalKeysP`
     [ ("M-b", spawn "vivaldi")
     , ("M-w", kill)
     , ("M-<Space>", spawn "dmenu_run")
     , ("M-f", sendMessage NextLayout)
-    --show time in notification
-    , ("M-t", spawn "dunstify Time \"$(date)\"")
+    , ("M-s", spawn "dunstify Time \"$(date)\"")
     , ("M-a", toggleSystray)
     ]
 
 myManageHook :: ManageHook
 myManageHook = manageSpawn <> composeAll
-    [ isDialog			    --> doFloat
-    , className =? "discord"	    --> doShift "chat"
-    , className =? "Spotify"	    --> doShift "music"
+    [ isDialog --> doFloat
+    , className =? "discord" --> doShift "chat"
+    , className =? "Spotify" --> doShift "entertainment"
+    , className =? "element-desktop" --> doShift "chat"
+    , className =? "Vivaldi-stable" --> doShift "browser"
+    , className =? "lxqt-config" --> doShift "settings"
     ]
 
-myLayoutHook = smartSpacingWithEdge 10 $ noBorders $ myLayouts
-myLayouts = tiled ||| Full
+myLayoutHook = smartSpacingWithEdge 7 $ noBorders $ myLayouts
   where
+    myLayouts = tiled ||| Full
     tiled   = Tall nmaster delta ratio
     nmaster = 1      -- Default number of windows in the master pane
     ratio   = 1/2    -- Default proportion of screen occupied by master pane
     delta   = 3/100  -- Percent of screen to increment by when resizing panes
 
-myWorkspaces = ["term", "web", "3", "4", "5", "6", "7", "music", "chat"]
+myWorkspaces = ["term", "browser", "3", "4", "5", "6", "entertainment", "chat", "settings"]
 
 toggleSystray = do
     spawn "killall stalonetray || stalonetray"
 
 myStartupHook :: X ()
 myStartupHook = do
-  spawnOnce "picom"
-  spawnOnce "nm-applet"
-  spawnOnce "setxkbmap -layout gb -variant altgr-intl"
+  spawnOnOnce "term" "tmux attach -t 0 || tmux new -s 0"
   spawnOnce "walset"
-  spawnOnOnce "term" "alacritty"
-  spawnOnOnce "web" "vivaldi"
+  spawnOnce "vivaldi"
   spawnOnce "discord"
   spawnOnce "spotify"
+  spawnOnce "lxqt-config"
   spawn "killall dunst; notify-send 'Reloaded configuration'"
+
